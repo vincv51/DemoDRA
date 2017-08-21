aede1a7318c50ee57e8325ba73531a71fb632e2b
vincv51 <vincv51@yahoo.fr>
2017-08-21 10:34:27 +0200

    Update apiv1.js

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var router = express.Router();
 var REQUEST = require('request');
@@ -11,7 +10,8 @@ var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?q={city nam
 /*appid=6b7b471967dd0851d0010cdecf28f829&units=imperial";*/
 
 exports.getWeather = function(req, res) {
-	var zip = req.query.zip;
+	/*var zip = req.query.zip;*/
+	var zip = req.query.name;
 	if( (zip === null) || (typeof(zip) === 'undefined') ) {
 		return res.status(400).send('zip missing');
 	}
